6e2ce91f4dc22f7d4646e8d9081d9bd716eccd68
Violetta2410 <claire.translator.no1@gmail.com>
2025-01-23 23:57:54 +0900

    Initial commit

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ from datetime import datetime, timedelta
 import pandas as pd
 from query_action import DatabaseSearch, ResponseGeneration, ResponseReview, NewsChatbot
 import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 # 페이지 설정
 st.set_page_config(
